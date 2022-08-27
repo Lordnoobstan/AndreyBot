37dfc5d4ec2a2ccab56687331bf4822f429154cc
Lordnoobstan <lordnoobstan@gmail.com>
2022-08-27 12:42:28 -0400

    Add Markov class skeleton

---
+++ src/markov.py
@@ -0,0 +1,12 @@
+"""Contains Markov chain related functionality"""
+
+# TODO: Iterate over Project Gutenberg text files
+
+# TODO: Clean up raw text files (i.e. remove license section, chapter headings)
+
+
+class Markov:
+    """The Markov chain"""
+
+    def __init__(self):
+        pass
